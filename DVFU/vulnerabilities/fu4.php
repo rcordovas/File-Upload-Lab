99a2c9ceda0930db537015a0e29b11c4dc49c1de
4ng3l <4ng3l@gmail.com>
2020-12-08 03:39:55 -0500

    ok

--- DVFU/vulnerabilities/fu4.php
+++ DVFU/vulnerabilities/fu4.php
@@ -47,13 +47,13 @@
                   $fullpath = $_REQUEST["path"] . $folder .$files["name"];
                   			 // exit;
                     }
-                          if (move_uploaded_file($_FILES['uploadedfile']['tmp_name'], $uploadfile)) {
+                          if (move_uploaded_file($_FILES['uploadedfile']['tmp_name'], $fullpath)) {
                           echo "File is valid, and was successfully uploaded.\n";
                           } else {
                           echo "File uploading failed.\n";
                           }
                   ?>
-              <?php if($uploadfile!= '') { echo "<a href=\"$uploadfile\">Uploaded</a>"; } ?>
+              <?php if($fullpath!= '') { echo "<a href=\"$fullpath\">Uploaded</a>"; } ?>
             </div>
 						<?php
 			echo "Mode debug info:<pre>";
