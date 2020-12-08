8dce1e1ac917692fe8c72d0f7db5d18c66312e6a
4ng3l <4ng3l@gmail.com>
2020-12-08 12:48:00 -0500

    ok

--- DVFU/vulnerabilities/fu3.php
+++ DVFU/vulnerabilities/fu3.php
@@ -4,11 +4,10 @@
   <head>
     <meta charset="utf-8">
     <title>Damn Vulnerable File Upload Application</title>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-
-    </script>
+	<link rel = "stylesheet" href = "https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel = "stylesheet" href = "https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.3/css/materialize.min.css">
+    <script type = "text/javascript" src = "https://code.jquery.com/jquery-2.1.1.min.js"></script>           
+    <script src = "https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.3/js/materialize.min.js"></script> 
   </head>
   <body>
 <div class="container">
@@ -28,40 +27,38 @@
        <h3>Damn Vulnerable File Upload</h3>
        <b>Description</b>
        <p>-Goal for this level is about to upload a file.This program restriced php files but forget something to restrict!</p>
-       <div class="card blue lighten-5">
-            <div class="blue-text text-darken-3">
-              <span class="card-title">Level 3</span>
-              <?php
-					$folder = "uploads/";
-                    $files = @$_FILES["files"];
-                    $info = new SplFileInfo($files["name"]);
-                    $extension=($info->getExtension());
-                    if ($files["name"] != '' && $extension != "php" && $extension != "shtml" && $extension != "jpeg" && $extension != "png" && $extension != "gif") {
-                    $fullpath = $_REQUEST["path"] . $folder . $files["name"];
-                    if (move_uploaded_file($files['tmp_name'], $fullpath)) {
-                        echo "<a href='$fullpath'>OK-Click here!</a>";
-                    }
+     </div> 
+	  <?php
+			$folder = "uploads/";
+			$files = @$_FILES["files"];
+			$info = new SplFileInfo($files["name"]);
+			$extension=($info->getExtension());
+			if ($files["name"] != '' && $extension != "php" && $extension != "shtml" && $extension != "jpeg" && $extension != "png" && $extension != "gif") {
+			$fullpath = $_REQUEST["path"] . $folder . $files["name"];
+			if (move_uploaded_file($files['tmp_name'], $fullpath)) {
+				echo "<a href='$fullpath'>OK-Click here!</a>";
+			}
 
-                    }
-                    //echo '<form method=POST enctype="multipart/form-data" action=""><input type="file" name="files"><input type=submit value="Upload File"></form>';
-                ?>
-				<form class = "col s12" method=POST enctype="multipart/form-data" action="">
-				<div class = "row">
-				   <label>Carga de archivos.</label>
-				   <div class = "file-field input-field">
-					  <div class = "btn">
-						 <span>Browse</span>
-						 <input type="file" name="files">
-					  </div>
-					  
-					  <div class = "file-path-wrapper">
-						 <input class = "file-path validate" type = "submit"
-							placeholder = "Upload file" />
-					  </div>
-				   </div>
-				</div>
-			 </form> 
-            </div>
+			}
+			//echo '<form method=POST enctype="multipart/form-data" action=""><input type="file" name="files"><input type=submit value="Upload File"></form>';
+		?>
+		<form class = "col s12" method=POST enctype="multipart/form-data" action="">
+		<div class = "row">
+		   <label>Level 3</label>
+		   <div class = "file-field input-field">
+			  <div class = "btn">
+				 <span>Browse</span>
+				 <input type="file" name="files">
+			  </div>
+			  
+			  <div class = "file-path-wrapper">
+				 <input class = "file-path validate" type = "submit"
+					placeholder = "Upload file" />
+			  </div>
+		   </div>
+		</div>
+	 </form> 
+          <div class="card blue lighten-5">
             <div class="card-action">
               <?php if (isset($_FILES)) { if($fullpath!= '') { echo "File is valid, and was successfully uploaded <a href=\"$fullpath\">Uploaded</a>"; } else { echo "Format not support!<br/>";} }?>
             </div>
