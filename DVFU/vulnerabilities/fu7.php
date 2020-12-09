71d3245acc6369567b61a2923e4f932cca027d47
4ng3l <4ng3l@gmail.com>
2020-12-08 23:01:32 -0500

    ok

--- DVFU/vulnerabilities/fu7.php
+++ DVFU/vulnerabilities/fu7.php
@@ -67,8 +67,9 @@
 	<div class="card light-blue darken-3">
 		<div class="card-content white-text">
 			<?php
-				echo $_GET['file'] . "<br/>";
-				print_r(file_get_contents($_GET['file'] , FILE_USE_INCLUDE_PATH));
+				echo "<pre>";
+				echo file_get_contents($_GET['file'] , FILE_USE_INCLUDE_PATH);
+				echo "</pre>";
 			}
 			?>
 		</div>
