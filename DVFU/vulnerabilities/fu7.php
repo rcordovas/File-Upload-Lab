4941ae338c1191a50c62ebc4a33d20eefb3dc146
4ng3l <4ng3l@gmail.com>
2020-12-08 22:55:06 -0500

    ok

--- DVFU/vulnerabilities/fu7.php
+++ DVFU/vulnerabilities/fu7.php
@@ -71,12 +71,14 @@
 	<div class="card light-blue darken-3">
 		<div class="card-content white-text">
 			<?php
-			echo $_GET['file'] . "<br/>";
+				echo $_GET['file'] . "<br/>";
 				print_r(file_get_contents($_GET['file'] , FILE_USE_INCLUDE_PATH));
 			}
 			?>
 		</div>
-    </div> 
+    </div>
+	</div>
+</div>
 </div>
 <style>
 	.custom-file-input::-webkit-file-upload-button {
